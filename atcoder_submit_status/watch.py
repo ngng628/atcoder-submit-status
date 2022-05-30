0ff0649dd766e68514c9ab78db4eb9670791ca19
ngng628 <s211068q@st.go.tuat.ac.jp>
2022-05-30 21:49:39 +0900

    :lipstick: info-modeをinfo-levelに命名変更

--- atcoder_submit_status/watch.py
+++ atcoder_submit_status/watch.py
@@ -25,14 +25,14 @@ Supported Services:
    subparser.add_argument('--languages', default=[], nargs='*', help='Select languages.\n(e.g. C++ C#)')
    subparser.add_argument('--statuses', default=[], nargs='*', choices=['AC', 'CE', 'MLE', 'TLE', 'RE', 'OLE', 'IE', 'WA'], help='Select statuses.\n(e.g. WA TLE)')
    subparser.add_argument('-u', '--users', default=[], nargs='*', help='Watch users.')
-   subparser.add_argument('--info-mode', default='NORMAL', choices=['MINIMAL', 'NORMAL', 'DETAILS'], help='表示の細かさを設定します')
+   subparser.add_argument('--info-level', default='NORMAL', choices=['MINIMAL', 'NORMAL', 'DETAILS'], help='表示の細かさを設定します')
    subparser.add_argument('-t', '--tail', default=sys.maxsize, type=int, help='Print the last $TAIL submissions')
 
 def _fetch(args: argparse.Namespace, service: service.Service, session: Optional[requests.Session] = None):
    session = session or utils.get_default_session()
    submissions = service.fetch_submissions(args.url, no_color=args.no_color, tasks=args.tasks, languages=args.languages, statuses=args.statuses, users=args.users, session=session)
-   if args.info_mode != 'DETAILS':
-      submissions = service.minimize_submissions_info(submissions, args.info_mode)
+   if args.info_level != 'DETAILS':
+      submissions = service.minimize_submissions_info(submissions, args.info_level)
    return submissions
 
 def _draw(args: argparse.Namespace, submissions):
